a3917697c6162259a32d91bd63dd5f9a94bd6bab
DOUGBOWMER <dougbowmer3@gmail.com>
2021-12-12 14:26:52 -0500

    initial commit

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -10,7 +10,7 @@ import "hardhat/console.sol";
 contract NFTMarket is ReentrancyGuard {
   using Counters for Counters.Counter;
   Counters.Counter private _itemIds;
-  Counters.Counter private _itemsSold;
+  Counters.Counter private _itemsSold; 
 
   address payable owner;
   uint256 listingPrice = 0.025 ether;
